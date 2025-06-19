83ea6d2b6f1df93d726dc558f193f40a3eebcbaa
Mridul Katara <mridul.katara@masaischool.com>
2025-06-19 21:19:19 +0530

    updated code

---
+++ frontend/common/profile/Profile.jsx
@@ -0,0 +1,154 @@
+
+
+const Frame1321316922 = () => {
+    return (
+          <div className="w-full relative bg-whitesmoke-200 h-[942px] overflow-hidden text-center text-sm text-gray-500 font-roboto">
+                <div className="absolute top-[0px] left-[calc(50%_-_187.5px)] w-[375px] h-11 flex flex-row items-center justify-between py-2.5 px-6 box-border gap-0 text-left">
+                      <div className="relative tracking-[0.01em] leading-5 font-medium">9:30</div>
+                      <img className="w-[46px] relative h-[17px]" alt="" src="right icons.svg" />
+                </div>
+                <div className="absolute top-[68px] left-[calc(50%_-_171.5px)] flex flex-col items-center justify-start gap-6 text-2xl text-black font-poppins">
+                      <div className="w-[343px] flex flex-col items-start justify-start gap-4">
+                            <div className="self-stretch flex flex-row items-center justify-between gap-0">
+                                  <div className="w-8 relative h-8 [transform:_rotate(180deg)]">
+                                        <img className="absolute h-3/6 w-[66.56%] top-[25%] right-[16.77%] bottom-[25%] left-[16.67%] max-w-full overflow-hidden max-h-full [transform:_rotate(-180deg)]" alt="" src="arrow_right_alt.svg" />
+                                  </div>
+                                  <div className="relative leading-[130%] font-semibold">Profile</div>
+                                  <div className="w-8 relative h-8 [transform:_rotate(180deg)]">
+                                        <img className="absolute h-3/6 w-[66.56%] top-[25%] right-[16.77%] bottom-[25%] left-[16.67%] max-w-full overflow-hidden max-h-full [transform:_rotate(-180deg)]" alt="" src="arrow_right_alt.svg" />
+                                  </div>
+                            </div>
+                            <div className="w-[343px] [backdrop-filter:blur(34px)] rounded-3xl bg-white flex flex-col items-center justify-start pt-6 px-4 pb-2 box-border relative gap-3 text-left text-xl text-gray-200 font-inter">
+                                  <img className="w-20 relative rounded-[150px] h-20 object-cover z-[0]" alt="" src="Avatar.png" />
+                                  <div className="self-stretch flex flex-col items-center justify-start z-[1]">
+                                        <div className="self-stretch flex flex-col items-start justify-start gap-6">
+                                              <div className="self-stretch flex flex-col items-center justify-start gap-2">
+                                                    <div className="relative leading-[120%] font-semibold">John Smith</div>
+                                                    <div className="relative text-xs leading-[120%] font-poppins text-darkslategray">john@iitmandi.ac.in</div>
+                                              </div>
+                                              <div className="self-stretch flex flex-col items-end justify-start gap-4 text-xs text-dimgray">
+                                                    <div className="self-stretch flex flex-col items-start justify-start">
+                                                          <div className="self-stretch flex flex-col items-start justify-start gap-0.5">
+                                                                <div className="relative leading-[120%]">Student Code</div>
+                                                                <div className="self-stretch relative text-sm leading-[140%] font-medium font-poppins text-gray-400">S123456</div>
+                                                          </div>
+                                                          <div className="self-stretch flex flex-col items-start justify-start pt-3.5 px-0 pb-0 gap-0.5">
+                                                                <div className="relative leading-[120%]">University</div>
+                                                                <div className="self-stretch relative text-sm leading-[140%] font-medium font-poppins text-gray-400">IIT Mandi</div>
+                                                          </div>
+                                                    </div>
+                                                    <div className="self-stretch flex flex-col items-start justify-start gap-4 text-sm text-gray-400 font-poppins">
+                                                          <div className="self-stretch rounded-2xl [background:linear-gradient(89.38deg,_#fff,_#fa5620)] border-orangered-100 border-solid border-[1px] flex flex-row items-start justify-start">
+                                                                <div className="flex-1 flex flex-row items-center justify-between py-3.5 px-3 gap-0">
+                                                                      <div className="relative leading-[140%] font-medium">My Community</div>
+                                                                      <div className="rounded-xl bg-white flex flex-row items-center justify-center py-1 px-2 text-center text-xs text-orangered-200">
+                                                                            <div className="relative leading-[120%] capitalize font-semibold">{`Coming Soon `}</div>
+                                                                      </div>
+                                                                </div>
+                                                          </div>
+                                                          <div className="self-stretch flex flex-col items-start justify-start gap-3 text-xs text-dimgray font-inter">
+                                                                <div className="self-stretch rounded-[20px] border-gainsboro border-solid border-[1px] flex flex-col items-start justify-start">
+                                                                      <div className="self-stretch border-gainsboro border-solid border-b-[1px] flex flex-col items-start justify-start py-3.5 px-3 gap-0.5">
+                                                                            <div className="relative leading-[120%]">Program Name</div>
+                                                                            <div className="self-stretch relative text-sm leading-[140%] font-medium font-poppins text-gray-400">Computer Science</div>
+                                                                      </div>
+                                                                      <div className="self-stretch border-gainsboro border-solid border-b-[1px] flex flex-col items-start justify-start py-3.5 px-3 gap-0.5">
+                                                                            <div className="relative leading-[120%]">Current Semester</div>
+                                                                            <div className="self-stretch relative text-sm leading-[140%] font-medium font-poppins text-gray-400">3</div>
+                                                                      </div>
+                                                                      <div className="self-stretch flex flex-col items-start justify-start py-3.5 px-3 gap-0.5">
+                                                                            <div className="relative leading-[120%]">Fees Status</div>
+                                                                            <div className="self-stretch relative text-sm leading-[140%] font-medium font-poppins text-darkgoldenrod">Pending</div>
+                                                                      </div>
+                                                                </div>
+                                                                <div className="self-stretch rounded-2xl border-gainsboro border-solid border-[1px] flex flex-col items-start justify-start">
+                                                                      <div className="self-stretch border-gainsboro border-solid border-b-[1px] flex flex-col items-start justify-start py-3.5 px-3 gap-0.5">
+                                                                            <div className="relative leading-[120%]">Program Name</div>
+                                                                            <div className="self-stretch relative text-sm leading-[140%] font-medium font-poppins text-gray-400">Business Analytics</div>
+                                                                      </div>
+                                                                      <div className="self-stretch border-gainsboro border-solid border-b-[1px] flex flex-col items-start justify-start py-3.5 px-3 gap-0.5">
+                                                                            <div className="relative leading-[120%]">Current Semester</div>
+                                                                            <div className="self-stretch relative text-sm leading-[140%] font-medium font-poppins text-gray-400">3</div>
+                                                                      </div>
+                                                                      <div className="self-stretch flex flex-col items-start justify-start py-3.5 px-3 gap-0.5">
+                                                                            <div className="relative leading-[120%]">Fees Status</div>
+                                                                            <div className="self-stretch relative text-sm leading-[140%] font-medium font-poppins text-green">Paid</div>
+                                                                      </div>
+                                                                </div>
+                                                          </div>
+                                                    </div>
+                                              </div>
+                                        </div>
+                                  </div>
+                                  <div className="!!m-[0 important] absolute top-[16px] right-[16px] flex flex-row items-start justify-start z-[2]">
+                                        <div className="flex flex-row items-center justify-start">
+                                              <div className="rounded-lg bg-whitesmoke-100 flex flex-row items-center justify-start p-2">
+                                                    <div className="w-6 relative h-6 overflow-hidden shrink-0">
+                                                          <img className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] max-w-full overflow-hidden max-h-full" alt="" src="Vector.svg" />
+                                                          <img className="absolute h-[68.75%] w-3/12 top-[15.63%] right-[56.25%] bottom-[15.63%] left-[18.75%] max-w-full overflow-hidden max-h-full" alt="" src="Vector.svg" />
+                                                          <img className="absolute h-[8.33%] w-[43.75%] top-[50%] right-[12.5%] bottom-[41.67%] left-[43.75%] max-w-full overflow-hidden max-h-full" alt="" src="Vector.svg" />
+                                                          <img className="absolute h-[31.25%] w-[15.83%] top-[34.38%] right-[12.29%] bottom-[34.38%] left-[71.88%] max-w-full overflow-hidden max-h-full" alt="" src="Vector.svg" />
+                                                    </div>
+                                              </div>
+                                        </div>
+                                  </div>
+                                  <div className="!!m-[0 important] absolute top-[16px] left-[16px] rounded-lg bg-whitesmoke-100 flex flex-row items-center justify-start p-2 z-[3]">
+                                        <div className="w-6 relative h-6">
+                                              <img className="absolute h-3/4 w-9/12 top-[12.5%] right-[12.5%] bottom-[12.5%] left-[12.5%] max-w-full overflow-hidden max-h-full" alt="" src="edit.svg" />
+                                        </div>
+                                  </div>
+                            </div>
+                      </div>
+                      <div className="w-[343px] [backdrop-filter:blur(34px)] rounded-xl bg-white flex flex-col items-center justify-start py-3 px-4 box-border text-left text-sm text-gray-600 font-inter">
+                            <div className="self-stretch flex flex-row items-center justify-between gap-0">
+                                  <div className="relative leading-[120%] font-medium">Payment History</div>
+                                  <div className="w-6 relative h-6">
+                                        <img className="absolute h-3/6 w-[66.67%] top-[25%] right-[16.67%] bottom-[25%] left-[16.67%] max-w-full overflow-hidden max-h-full" alt="" src="arrow_right_alt.svg" />
+                                  </div>
+                            </div>
+                      </div>
+                </div>
+                <div className="absolute bottom-[0px] left-[calc(50%_-_187.5px)] w-[375px] h-[92px] text-xs text-gray-100">
+                      <div className="absolute bottom-[0px] left-[calc(50%_-_187.5px)] bg-white w-[375px] h-6">
+                            <div className="absolute top-[calc(50%_-_2px)] left-[calc(50%_-_53.5px)] rounded-xl bg-gray-300 w-[108px] h-1" />
+                      </div>
+                      <div className="absolute bottom-[24px] left-[calc(50%_-_187.5px)] bg-white border-whitesmoke-200 border-solid border-t-[1px] box-border w-[375px] flex flex-row items-start justify-start py-0 px-2 gap-2">
+                            <div className="flex-1 flex flex-col items-center justify-start py-2 px-0 gap-1">
+                                  <div className="rounded-2xl overflow-hidden flex flex-col items-center justify-center">
+                                        <div className="w-16 h-8 flex flex-row items-center justify-center py-1 px-5 box-border">
+                                              <div className="w-6 relative h-6">
+                                                    <img className="absolute h-3/4 w-[66.67%] top-[12.5%] right-[16.67%] bottom-[12.5%] left-[16.67%] max-w-full overflow-hidden max-h-full" alt="" src="home.svg" />
+                                              </div>
+                                        </div>
+                                  </div>
+                                  <div className="self-stretch relative leading-[130%] font-medium">Home</div>
+                            </div>
+                            <div className="flex-1 flex flex-col items-center justify-center py-2 px-0 gap-1">
+                                  <div className="w-8 rounded-2xl flex flex-row items-center justify-center">
+                                        <div className="w-16 h-8 flex flex-row items-center justify-center py-1 px-5 box-border">
+                                              <div className="w-6 relative h-6">
+                                                    <img className="absolute h-[83.33%] w-[66.67%] top-[8.33%] right-[16.67%] bottom-[8.33%] left-[16.67%] max-w-full overflow-hidden max-h-full" alt="" src="book_2.svg" />
+                                              </div>
+                                        </div>
+                                  </div>
+                                  <div className="self-stretch relative leading-[130%] font-medium">My Program</div>
+                            </div>
+                            <div className="w-[72.8px] h-[76px] hidden" />
+                            <div className="flex-1 flex flex-col items-center justify-center py-2 px-0 gap-1">
+                                  <img className="w-8 rounded-2xl h-8" alt="" src="icon-container.svg" />
+                                  <div className="self-stretch relative leading-[130%] font-medium">Notification</div>
+                            </div>
+                            <div className="flex-1 flex flex-col items-center justify-center py-2 px-0 gap-1 text-mediumslateblue">
+                                  <div className="w-8 rounded-2xl flex flex-row items-center justify-center">
+                                        <div className="w-16 h-8 flex flex-row items-center justify-center py-1 px-5 box-border">
+                                              <img className="w-6 relative rounded-[45px] h-6 object-cover" alt="" src="Avatar.png" />
+                                        </div>
+                                  </div>
+                                  <div className="self-stretch relative leading-[130%] font-medium">Profile</div>
+                            </div>
+                      </div>
+                </div>
+          </div>);
+};
+
+export default Frame1321316922;
