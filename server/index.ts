4ef0409717e5980df379ab1bf903e3e3ed9c176e
Nihal <41384815+niftynihal@users.noreply.github.com>
2025-06-25 19:17:29 +0530

    Removed unnecessary configs in cors middleware

--- server/index.ts
+++ server/index.ts
@@ -11,10 +11,6 @@ connectDB()
 const app = new Elysia()
 .use(cors({
   origin: '*',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
-  exposeHeaders: ['Content-Type', 'Authorization'],
 }))
   .use(authRoutes)
   .use(programRoutes)
